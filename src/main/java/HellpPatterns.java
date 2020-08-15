1055bde6cf82e5ed0cf31d505fa1553ed2750084
licj <1392252267@qq.com>
2020-08-15 16:50:03 +0800

    Initial commit

---
+++ src/main/java/HellpPatterns.java
@@ -0,0 +1,11 @@
+/**
+ * @author licjd
+ * @date 2020/08/15
+ */
+public class HellpPatterns {
+
+    public static void main(String[] args) {
+        System.out.printf("Hello Patterns!");
+    }
+
+}
